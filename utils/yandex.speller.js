50062e751b741fe1d497051eab777e006709c31c
AndrewGolovchenko <drmorzek@mail.ru>
2021-11-28 21:29:20 +0300

    добавил мемоизацию

--- utils/yandex.speller.js
+++ utils/yandex.speller.js
@@ -1,23 +1,28 @@
 const requestHTTP = require('./request');
 
+const cache = {}
+
 module.exports = async function (text) {
   try {
       let newText = text
       let chunks = text.replace(/\r\n|\n\r|\n|\r/g, "\n").split(/\s+|\n/).filter(e => e !== '')
       chunks = [...new Set(chunks)]
+    
+      let chunksFromCache = chunks.filter(e => !cache[e] )
+
       let url = "https://speller.yandex.net/services/spellservice.json/checkText?"
-      let arr = []
 
-      while (chunks.length > 0) {
-          let res = await requestHTTP(url + "text=" + chunks.splice(0, 20).join(" "))
-          arr = arr.concat(res)
+      while (chunksFromCache.length > 0) {
+          let res = await requestHTTP(url + "text=" + chunksFromCache.splice(0, 20).join(" "))
+          res.forEach( e => {
+              cache[e.word] = e.s[0] || e.word
+          })
       }
-
       
-      arr.forEach(function(elem) {
+      chunks.forEach(function(elem) {
+          if(!cache[elem]) cache[elem] = elem
           newText = newText.replaceAll(
-              elem.word,
-              elem.s[0] || elem.word
+            elem, cache[elem] 
           )
       })
       
